34f2515819d1034e06aac2068577116e8866a08a
JavFuentes <javier.antonio8128@gmail.com>
2025-09-04 18:49:44 -0400

    primer commit

---
+++ app/src/main/java/dev/javfuentes/login/CamaraFragment.java
@@ -0,0 +1,20 @@
+package dev.javfuentes.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CamaraFragment extends Fragment {
+    public CamaraFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camara, container, false);
+    }
+}
--- app/src/main/java/dev/javfuentes/login/MainActivity.java
+++ app/src/main/java/dev/javfuentes/login/MainActivity.java
@@ -1,24 +1,45 @@
 package dev.javfuentes.login;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import dev.javfuentes.login.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(binding.navHostFragment.getId());
+
+        if (navHostFragment != null) {
+
+            NavController navController = ((NavHostFragment) navHostFragment).getNavController();
+
+            NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        }
     }
 }
